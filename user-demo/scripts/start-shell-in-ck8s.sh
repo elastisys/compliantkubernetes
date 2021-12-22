d6739493d69e3537c3cace15958f89a355413080
Cristian Klein <cristian.klein@elastisys.com>
2021-12-22 16:58:19 +0100

    Add script to start a shell in Compliant Kubernetes

---
+++ user-demo/scripts/start-shell-in-ck8s.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+IMAGE=alpine/curl
+CMD=sh
+
+OVERRIDES='''
+{
+  "apiVersion": "v1",
+  "spec": {
+     "containers": [{
+       "name": "shell",
+       "image": "'$IMAGE'",
+       "args": ["'$CMD'"],
+       "tty": true,
+       "stdin": true,
+       "securityContext": {
+         "runAsUser": 1000
+       },
+       "resources": {
+         "requests": {
+            "cpu": "100m",
+            "memory": "100m"
+         }
+       }
+    }]
+  }
+}
+'''
+
+kubectl run --rm -ti shell-$USER --overrides="$OVERRIDES" --image=overriden -- overriden
